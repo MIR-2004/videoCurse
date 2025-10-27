65f5ba69fc6debe6e785869452db5e9e5799c612
Pahari47 <nachiketapahari00@gmail.com>
2025-10-27 18:02:19 +0530

    fix multer bug

--- server/src/routes/editJobRouter.ts
+++ server/src/routes/editJobRouter.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { createEditJob } from "../controllers/videoController";
+import { upload } from "../config/multer";
 
 const router = Router();
 
-router.post("/", createEditJob);
+router.post("/", upload.single("video"), createEditJob);
 
 export default router;
